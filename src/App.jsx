6cd61b0a14f7ce933ddf77ca1d0fb156aa2a6342
Junaid8217 <junaidhossain77349@gmail.com>
2025-10-05 18:36:21 -0700

    Added toggling feature

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const fetchPlayers = async () => {
 }
 
 function App() {
+  const [toggle, setToggle] = useState(true) 
 
   const playerPromise = fetchPlayers()
 
@@ -22,12 +23,21 @@ function App() {
       
 
       <Navbar></Navbar>
-      
-      <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
-        <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
-      </Suspense>
-      {/* <SelectedPlayers></SelectedPlayers> */}
+      <div className=' max-w-[1200px] mx-auto flex justify-between items-center'>
+        <h1 className='font-bold text-2xl'>Available Players</h1>
+        <div className='font-bold'>
+          <button onClick={()=> setToggle(true)} className={`p-3 border-1 border-gray-400 rounded-l-2xl border-r-0 ${toggle===true? "bg-green-200": ""}`}>Available</button>
+          <button onClick={()=> setToggle(false)} className={`p-3 border-1 border-gray-400 rounded-r-2xl border-r-0 ${toggle===false? "bg-green-200": ""}`}>Selected <span>(0)</span></button>
+        </div>
+      </div>
 
+      {
+        toggle === true? <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+        <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
+      </Suspense> : <SelectedPlayers></SelectedPlayers>
+      }
+     
+     
 
 
     </>
--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SelectedPlayers = () => {
     return (
-        <div>
+        <div >
             
         </div>
     );
