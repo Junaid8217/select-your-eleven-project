f2a5bed064743a726de445200e84d7fdfc924c3b
Junaid8217 <junaidhossain77349@gmail.com>
2025-10-05 17:44:42 -0700

    Added 12 card of players and made it responsive

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -8,42 +8,47 @@ const AvailablePlayers = ({ playerPromise }) => {
     const playerData = use(playerPromise)
     console.log(playerData)
     return (
-        <div className='max-w-[1200px] mx-auto'>
-            <div className="card bg-base-100 w-96 shadow-sm p-4">
+        <div className='max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 '>
+
+            {
+                playerData.map(player => <div className="card bg-base-100 shadow-sm p-4">
                 <figure>
                     <img
-                        src="https://i.ibb.co.com/JRDckGqv/1696748634550-Shakib-Al-Hasan.jpg"
-                        alt="Shoes" />
+                        src={player.player_image}
+                        alt="Image unavailable" />
                 </figure>
-                <div className="mt-4">
+                <div className="mt-4 ">
                     <div className='flex'>
                         <img src={userImg} alt="" />
-                    <h2 className="card-title ml-2">Shakib All Hasan</h2>
+                    <h2 className="card-title ml-2">{player.player_name}</h2>
                     </div>
                     <div className='flex justify-between mt-4 border-b-1 pb-2 border-gray-400'>
                         <div className='flex gap-2 items-center'>
                             <img className='w-[20px] h-[20px] ' src={flagImg} alt="" />
-                            <span>India</span>
+                            <span>{player.player_country}</span>
                         </div>
                         <button className='btn'>All Rounder</button>
                     </div>
 
                     <div className='flex justify-between font-bold mt-4'>
                         <span>Ratting</span>
-                        <span>5</span>
+                        <span>{player.rating}</span>
                     </div>
                     <div className='flex justify-between '>
-                        <span className='font-bold'>Left Hand Bat</span>
-                        <span>Left Hand Bowl</span>
+                        <span className='font-bold'>{player.batting_style}</span>
+                        <span>{player.bowling_style}</span>
                     </div>
 
 
                     <div className="card-actions  mt-3 justify-between items-center">
-                        <p>Price: $1500000</p>
+                        <p>Price: <span>{player.price}</span></p>
                         <button className="btn ">Choose Player</button>
                     </div>
                 </div>
-            </div>
+            </div>)
+            }
+
+            
 
         </div>
     );
